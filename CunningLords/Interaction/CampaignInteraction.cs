3e37f8777080bfb979f4f43baffb0c92a3edbc6f
AshenWaltz <jquadrado@live.com.pt>
2021-04-27 20:02:01 +0100

    Added Simple interacion within Campaign

---
+++ CunningLords/Interaction/CampaignInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.Engine;
+using TaleWorlds.InputSystem;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using CunningLords.Patches;
+
+namespace CunningLords.Interaction
+{
+    class CampaignInteraction
+    {
+
+        [HarmonyPatch(typeof(Campaign))]
+        [HarmonyPatch("RealTick")]
+        class CampaignHourlyTickOverride
+        {
+            static void Postfix(Campaign __instance)
+            {
+                if (Input.IsKeyDown(InputKey.Numpad7))
+                {
+                    InformationManager.DisplayMessage(new InformationMessage("PRESSED!"));
+                }
+            }
+        }
+
+    }
+}
