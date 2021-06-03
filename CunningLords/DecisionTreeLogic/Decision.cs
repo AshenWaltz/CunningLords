e3bd56950a89a934687ac0628adacdd021af7dd5
AshenWaltz <jquadrado@live.com.pt>
2021-06-03 21:28:38 +0100

    Added Basic implementation for Decision Tree

---
+++ CunningLords/DecisionTreeLogic/Decision.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+
+namespace CunningLords.DecisionTreeLogic
+{
+    public class Decision : DecisionTreeNode
+    {
+        protected DecisionTreeNode trueNode;
+
+        protected DecisionTreeNode falseNode;
+
+        public Decision(Formation f, ref DecisionTreeNode tn, ref DecisionTreeNode fn) : base(f)
+        {
+            this.formation = f;
+            this.trueNode = tn;
+            this.falseNode = fn;
+        }
+
+        private DecisionTreeNode getBranch()
+        {
+            return this.trueNode;
+        }
+
+        public override void makeDecision()
+        {
+            DecisionTreeNode chosen = this.getBranch();
+            chosen.makeDecision();
+        }
+    }
+}
---
+++ CunningLords/DecisionTreeLogic/DecisionTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+
+namespace CunningLords.DecisionTreeLogic
+{
+    public abstract class DecisionTreeNode
+    {
+        protected Formation formation;
+        public DecisionTreeNode(Formation f)
+        {
+            this.formation = f;
+        }
+
+        public abstract void makeDecision();
+    }
+}
